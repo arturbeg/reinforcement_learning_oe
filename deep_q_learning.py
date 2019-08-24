6e712c38758dbe40e97bbbbb863215d5dccf2d6e
Artur Begyan <arturbegyan98@gmail.com>
2019-08-24 22:11:36 +0100

    notes

--- deep_q_learning.py
+++ deep_q_learning.py
@@ -34,8 +34,6 @@ class Env(object):
     return (self.state, reward, is_done)
 
   def is_done():
-
-
     return self.state.inventory == 0
 
   def get_price(self, time):
@@ -51,64 +49,39 @@ def run():
   df_raw = pd.read_csv('./Data_Sets/APPL10minTickData.csv', header=0)
   prices = np.array(df_raw['close'].to_numpy)
 
-  initial_state = State(inventory=1000, time=0)
+  initial_state = State(inventory=100, time=0)
   env = Env(state=initial_state, prices=prices)
 
-  inventory = 1000
-  time  = 100 # multiples of 10 minutes
-  state_space = np.array([time, inventory])
-  action_space = np.array(list(range(0, len(inventory))))
-  state_size = len(state_space)
-  action_size = len(action_space)
+  state_size = 2
+  action_size = env.state.inventory
   agent = DQNAgent(state_size, action_size)
-  # agent.load("./save/cartpole-dqn.h5")
+
   done = False
   batch_size = 32
-  price = np.array([]) #insert full price time series
 
-  print("training is starting")
-  for e in range(EPISODES):
-      state = state_space
-      state = np.reshape(state, [1, state_size])  # what's the point??? TODO
-      for time in range(500):
-          action = agent.act(state)
-          next_state = (inventory - action)
-          price_over_t = price[(time-1):(time+noOfSteps)] #price vector should include the following times [t-1, t, t+1, ..., t+noOfSteps]
-          # if time%%12 == 0
-          #   action = agent.act(state)
-              reward = agent.reward(next_state, action, price_over_t,noOfSteps)
-          # else:
-          #   reward = agent.reward(next_state, action/12, price_over_t,noOfSteps)
-          reward = agent.reward(next_state, action, price_over_t,noOfSteps)
-          next_state = np.reshape(next_state, [1, state_size])  # ??? TODO
-          done = True if inventory == 0 else False
-          agent.remember(state, action, reward, next_state, done)
-          state = next_state
-          if done:
-              print("episode: {}/{}, score: {}, e: {:.2}"
-                    .format(e, EPISODES, time, agent.epsilon))
-              break
-          if len(agent.memory) > batch_size:
-              agent.replay(batch_size)
-      # if e % 10 == 0:
-      #     agent.save("./save/cartpole-dqn.h5")
-      print("episode is finished")
-
+  # for e in range(EPISODES):
+  
 class DQNAgent:
-    def __init__(self, state_size, action_size):
+    def __init__(self, state_size, action_size, time_to_execute):
         self.state_size = state_size
         self.action_size = action_size
         self.memory = deque(maxlen=10000)
-        self.gamma = 0.95 # discount rate
+        self.gamma = 0.99 # discount rate
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
         self.learning_rate = 0.001
+        self.time_to_execute = 42 # multiples of 10 minutes: 7 hours
         self.model = self._build_model()
 
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
         model = Sequential()
+        # TODO: update the architecutre --> <=6 layers
+        # Geometric pyramid rule
+        # Start with uniform for e-greedy, then go Binomial
+        # normalise (time)
+        # https://www.quora.com/What-are-some-rules-of-thumb-for-training-neural-networks
         model.add(Dense(24, input_dim=self.state_size, activation='relu'))
         model.add(Dense(24, activation='relu'))
         model.add(Dense(self.action_size, activation='linear'))
