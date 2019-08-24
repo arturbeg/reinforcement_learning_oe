ca3403cf5d2cdac053caa22d1b255c7d311d3dae
Artur Begyan <arturbegyan98@gmail.com>
2019-08-24 18:56:23 +0100

    readme

--- deep_q_learning.py
+++ deep_q_learning.py
@@ -8,9 +8,51 @@ from keras.layers import Dense
 from keras.optimizers import Adam
 import pandas as pd
 
-df_raw = pd.read_csv('./Data_Sets/APPL10minTickData.csv', header=0)
-prices = np.array(df_raw['close'].to_numpy)
+EPISODES = 1000
 
+def run():
+  df_raw = pd.read_csv('./Data_Sets/APPL10minTickData.csv', header=0)
+  prices = np.array(df_raw['close'].to_numpy)
+
+  inventory = 1000
+  time  = 10 #Hours
+  noOfSteps = 12
+  state_space = np.array([time, inventory])
+  action_space = np.array(list(range(0, len(inventory))))
+  state_size = len(state_space)
+  action_size = len(action_space)
+  agent = DQNAgent(state_size, action_size)
+  # agent.load("./save/cartpole-dqn.h5")
+  done = False
+  batch_size = 32
+  price = np.array([]) #insert full price time series
+
+  print("training is starting")
+  for e in range(EPISODES):
+      state = state_space
+      state = np.reshape(state, [1, state_size])  # what's the point??? TODO
+      for time in range(500):
+          action = agent.act(state)
+          next_state = (inventory - action)
+          price_over_t = price[(time-1):(time+noOfSteps)] #price vector should include the following times [t-1, t, t+1, ..., t+noOfSteps]
+          reward = agent.reward(next_state, action, price_over_t,noOfSteps)
+          next_state = np.reshape(next_state, [1, state_size])  # ??? TODO
+          done = True if inventory == 0 else False
+          agent.remember(state, action, reward, next_state, done)
+          state = next_state
+          if done:
+              print("episode: {}/{}, score: {}, e: {:.2}"
+                    .format(e, EPISODES, time, agent.epsilon))
+              break
+          if len(agent.memory) > batch_size:
+              agent.replay(batch_size)
+      # if e % 10 == 0:
+      #     agent.save("./save/cartpole-dqn.h5")
+      print("episode is finished")
+
+
+# class Env(object):
+  
 class DQNAgent:
     def __init__(self, state_size, action_size):
         self.state_size = state_size
@@ -71,43 +113,5 @@ class DQNAgent:
     def save(self, name):
         self.model.save_weights(name)
 
-EPISODES = 1000
 if __name__ == "__main__":
-    inventory = 1000
-    time  = 10 #Hours
-    noOfSteps = 12
-    state_space = np.array([time, inventory])
-    action_space = np.array(list(range(0, len(inventory))))
-    state_size = len(state_space)
-    action_size = len(action_space)
-    agent = DQNAgent(state_size, action_size)
-    # agent.load("./save/cartpole-dqn.h5")
-    done = False
-    batch_size = 32
-    price = np.array([]) #insert full price time series
-
-    print("training is starting")
-    for e in range(EPISODES):
-        state = state_space
-        state = np.reshape(state, [1, state_size])  # what's the point??? TODO
-        for time in range(500):
-            action = agent.act(state)
-            next_state = (inventory - action)
-            price_over_t = price[(time-1):(time+noOfSteps)] #price vector should include the following times [t-1, t, t+1, ..., t+noOfSteps]
-            reward = agent.reward(next_state, action, price_over_t,noOfSteps)
-            next_state = np.reshape(next_state, [1, state_size])  # ??? TODO
-            done = True if inventory == 0 else False
-            agent.remember(state, action, reward, next_state, done)
-            state = next_state
-            if done:
-                print("episode: {}/{}, score: {}, e: {:.2}"
-                      .format(e, EPISODES, time, agent.epsilon))
-                break
-            if len(agent.memory) > batch_size:
-                agent.replay(batch_size)
-        # if e % 10 == 0:
-        #     agent.save("./save/cartpole-dqn.h5")
-        print("episode is finished")
-
-
-    
+  run()
